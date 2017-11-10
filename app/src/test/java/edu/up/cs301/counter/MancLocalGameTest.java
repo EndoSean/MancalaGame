5280c253ab59e9f73992808daf0b909a976a5a0e
endo18 <endo18@up.edu>
2017-11-09 18:16:22 -0800

    Junit Tests

---
+++ app/src/test/java/edu/up/cs301/counter/MancLocalGameTest.java
@@ -0,0 +1,84 @@
+package edu.up.cs301.counter;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by endo18 on 11/9/2017.
+ */
+public class MancLocalGameTest {
+    @Test
+    public void canMove() throws Exception {
+
+    }
+
+    @Test
+    public void makeMove() throws Exception {
+
+    }
+
+    @Test
+    public void sendUpdatedStateTo() throws Exception {
+
+    }
+
+    @Test
+    public void checkIfGameOver() throws Exception {
+
+    }
+
+    @Test
+    public void testCanMove(){
+        MancLocalGame testing = new MancLocalGame();
+        //MancState test = new MancState();
+        int player = 1;
+        boolean thing = testing.canMove(player);
+        assertTrue(thing);
+
+        MancLocalGame testing2 = new MancLocalGame();
+        //MancState test = new MancState();
+        int player2 = -1;
+        boolean thing2 = testing2.canMove(player2);
+        assertTrue(thing);
+    }
+
+    @Test
+    public void testCheckIfGameOver(){
+        MancLocalGame test = new MancLocalGame();
+        MancState testcase = new MancState();
+        int[][] end = new int[2][7];
+        for(int i=0; i<2; i++){
+            for(int j=0; j<7; j++){
+                if(i==1){
+                    end[i][j] = 0;
+                }
+                else {
+                    end[i][j] = 1;
+                }
+            }
+        }
+        testcase.setMarble_Pos(end);
+        String thing = test.checkIfGameOver();
+        assertEquals(thing, "player1 has won.");
+
+    }
+
+    @Test
+    public void testMakeMove(){
+        MancLocalGame test = new MancLocalGame();
+        GamePlayer player = new GamePlayer();
+        boolean confirm = false;
+        MancMoveAction action = new MancMoveAction(player,confirm);
+        //action.confirm(1,18);
+        boolean thing = test.makeMove(action);
+        assertTrue(thing);
+
+        CounterHumanPlayer player2 = new CounterHumanPlayer();
+        boolean confirm2 = false;
+        CounterMoveAction action = new CounterMoveAction(player2,confirm2);
+        assertTrue(!thing);
+
+    }
+
+}
---
+++ app/src/test/java/edu/up/cs301/counter/MancStateTest.java
@@ -0,0 +1,72 @@
+package edu.up.cs301.counter;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by endo18 on 11/9/2017.
+ */
+public class MancStateTest {
+    @Test
+    public void setPlayer_Turn() throws Exception {
+
+    }
+
+    @Test
+    public void getPlayer_Turn() throws Exception {
+
+    }
+
+    @Test
+    public void setPlayer0_Score() throws Exception {
+
+    }
+
+    @Test
+    public void getPlayer0_Score() throws Exception {
+
+    }
+
+    @Test
+    public void setPlayer1_Score() throws Exception {
+
+    }
+
+    @Test
+    public void getPlayer1_Score() throws Exception {
+
+    }
+
+    @Test
+    public void setMarble_Pos() throws Exception {
+
+    }
+
+    @Test
+    public void getMarble_Pos() throws Exception {
+
+    }
+
+    @Test
+    public void testMancState(){
+        MancState test = new MancState();
+        test.setPlayer0_Score(1);
+        test.setPlayer1_Score(5);
+        test.setPlayer_Turn(1);
+        MancState copy = new MancState(test);
+        assertEquals(copy.getPlayer0_Score(),1);
+        assertEquals(copy.getPlayer1_Score(),5);
+        assertEquals(copy.getPlayer_Turn(),1);
+
+        MancState test2 = new MancState();
+        test.setPlayer0_Score(-1);
+        test.setPlayer1_Score(-5);
+        test.setPlayer_Turn(-1);
+        MancState copy2 = new MancState(test);
+        assertEquals(copy2.getPlayer0_Score(),-1);
+        assertEquals(copy2.getPlayer1_Score(),-5);
+        assertEquals(copy2.getPlayer_Turn(),-1);
+    }
+
+}
